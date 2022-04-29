07705a3d6944c0b5e3c8ad2723fe40f3290b9c10
Aneesh-Wable <89678773+Aneesh-Wable@users.noreply.github.com>
2022-04-29 20:11:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+prediction_1 = "";
+prediction_2 = "";
+
+Webcam.set({
+
+    width : 400,
+    height : 300,
+    img_format : 'png',
+    img_quality : 90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach("camera");
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri)
+    {
+        document.getElementById("result").innerHTML = '<img id = "captured_image"  src="'+data_uri+'">'
+    });
+}
